8f111752e03586fe11b19be8c470dc9bf2687100
Yovany Cunha <yovany33@gmail.com>
2025-08-23 20:28:48 -0300

    feat(products-render): first components - header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./header.css";
+
+export function Header() {
+  return (
+    <header className="wrapper">
+      <div className="container">
+        <p className="text">Products List</p>
+      </div>
+    </header>
+  );
+}
