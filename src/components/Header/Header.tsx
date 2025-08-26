e3ee0244064685a332293d8f2291b09326022d76
Yovany Cunha <yovany33@gmail.com>
2025-08-26 00:09:57 -0300

    feat(product-details): show last viewed product - missing tests

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,29 @@
+"use client";
+
+import { useAppSelector } from "@/lib/hooks";
 import "./header.css";
 
 export function Header() {
+  const { selectedProduct } = useAppSelector((state) => state.productSlice);
+
+  const renderNoSelectedMsg = () => (
+    <div className="seletedContainer">
+      <span className="selectedText">No products were seleted yet</span>
+    </div>
+  );
+
+  const renderSelectedProduct = () => (
+    <div className="selectedContainer">
+      <span className="selectedText">Recently viewed product:</span>
+      <span className="selectedProduct">{selectedProduct?.title}</span>
+    </div>
+  );
   return (
-    <header className="wrapper">
-      <div className="container">
-        <p className="text">Products List</p>
+    <header className="headerWrapper">
+      <div className="headerContainer">
+        <p className="headerText">Products List</p>
       </div>
+      {selectedProduct ? renderSelectedProduct() : renderNoSelectedMsg()}
     </header>
   );
 }
